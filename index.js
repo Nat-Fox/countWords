dd1b0d0b5fd787800a70d68cbce58b8d001636a0
nat-fox <natgramirez@outlook.com>
2018-02-27 15:40:50 -0300

    agregando contador de caracteres

--- index.js
+++ index.js
@@ -9,18 +9,16 @@ const call1Manual = require('./call1-manual.json');
 const call2Manual = require('./call2-manual.json');
 const Manual2 = require('./2-google-manual.json');
 
-googleSpeech = (googleTranscription) => {
+googleSpeechWords = (googleTranscription) => {
     let partsList = googleTranscription[0].results.map(part => part.alternatives[0].transcript);
     let conversation = partsList.join(' ');
-    let words = conversation.split(' ');
+    let words = conversation.split(' ');    
     let long = words.length;
-    //console.log(conversation.join(' ').split(' ').length)
-    //console.log(JSON.stringify(words, null, 4))
     
     return long
 }
 
-manualTranscription = (manualTranscription) => {
+manualTranscriptionWords = (manualTranscription) => {
     let partsListManual = manualTranscription[0].resultsManual.map(part => part.line[0].transcript);
     let conversationManual = partsListManual.join(' ');
     let wordsManual = conversationManual.split(' ');
@@ -29,12 +27,41 @@ manualTranscription = (manualTranscription) => {
     return longManual;
 }
 
+googleSpeechCharacters = (googleTranscription) => {
+    let partsList = googleTranscription[0].results.map(part => part.alternatives[0].transcript);
+    return partsList.join('').split('').length;    
+}
 
-console.log('Call 1 Google Speech API ->', googleSpeech(call1));
-console.log('Call 1 Manual ->', manualTranscription(call1Manual));
+manualTranscriptionCharacters = (manualTranscription) => {
+    let partsListManual = manualTranscription[0].resultsManual.map(part => part.line[0].transcript);
+    return partsListManual.join('').split('').length;
+} 
 
-console.log('Call 2 Google Speech API  ->', googleSpeech(call2));
-console.log('Call 2 Manual ->', manualTranscription(call2Manual));
 
-console.log('2 Google Speech API ->', googleSpeech(call2Google));
-console.log('2 Manual ->', manualTranscription(Manual2))
+// call1.flac
+console.log('call1.flac')
+console.log('Words call1.flac [Google Speech API] ->', googleSpeechWords(call1));
+console.log('Characters call1.flac [Google Speech API] -> ', googleSpeechCharacters(call1))
+
+console.log('Words call1.flac [MANUAL] ->', manualTranscriptionWords(call1Manual));
+console.log('Characters call1.flac [MANUAL]', manualTranscriptionCharacters(call1Manual));
+
+// call2.flac
+console.log('call2.flac')
+console.log('Words call2.flac [Google Speech API] ->', googleSpeechWords(call2));
+console.log('Characters call2.flac [Google Speech API] -> ', googleSpeechCharacters(call2));
+
+console.log('Words call2.flac [MANUAL] ->', manualTranscriptionWords(call2Manual));
+console.log('Characters call2.flac [MANUAL]', manualTranscriptionCharacters(call2Manual));
+
+// 2.flac
+console.log('2.flac')
+console.log('Words 2.flac [Google Speech API] ->', googleSpeechWords(call2Google));
+console.log('Characters call2.flac [Google Speech API] -> ', googleSpeechCharacters(call2Google));
+
+console.log('Words 2.flac [MANUAL] ->', manualTranscriptionWords(Manual2));
+console.log('Characters 2.flac [MANUAL]', manualTranscriptionCharacters(Manual2));
+
+
+
+
